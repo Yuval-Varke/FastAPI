edf6cec276fe447ae8763ccce6efe1b71b59b283
Yuval Varke <varke.vishu01@gmail.com>
2026-03-14 10:28:34 +0530

    get all products

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from service.products import get_all_products
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "FastAPI is running"}
+
+
+@app.get('/products')
+def get_products():
+    return get_all_products()
